c46f64a0263f209f130f837e3a663c16b79815ac
Aidan Feldman <aidan.feldman@gsa.gov>
2015-11-22 14:15:34 -0500

    clean the dist/ directory before build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var template = require('gulp-template');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var pkg = require('./package.json');
 
-gulp.task('default', function() {
+gulp.task('clean', function () {
+  return gulp.src('dist/*', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('build-css', ['clean'], function() {
   return gulp.src('bootstrap-toc.css')
     .pipe(template(pkg))
     .pipe(minifyCss({compatibility: 'ie8'}))
@@ -13,3 +19,6 @@ gulp.task('default', function() {
     }))
     .pipe(gulp.dest('dist'));
 });
+
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/running-tasks-in-series.md
+gulp.task('default', ['build-css']);

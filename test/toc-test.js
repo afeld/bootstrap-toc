ab73af2a806e1dc03174bacd761c180a006b459d
Aidan Feldman <aidan.feldman@gsa.gov>
2015-11-22 16:21:42 -0500

    add tests for generateUniqueId()

--- test/toc-test.js
+++ test/toc-test.js
@@ -1,3 +1,7 @@
+afterEach(function() {
+  $('#mocha-fixture').empty();
+});
+
 describe('Toc.helpers', function() {
   describe('#generateUniqueIdBase()', function() {
     it("uses the text from the element", function() {
@@ -11,6 +15,22 @@ describe('Toc.helpers', function() {
       var el = document.createElement('h1');
       var base = Toc.helpers.generateUniqueIdBase(el);
       expect(base).to.eql('h1');
+    });
+  });
+
+  describe('#generateUniqueId()', function() {
+    it("uses the tag name", function() {
+      var el = document.createElement('h1');
+      var base = Toc.helpers.generateUniqueId(el);
+      expect(base).to.eql('h1');
+    });
+
+    it("adds a suffix when there's an existing element with that tag", function() {
+      $('#mocha-fixture').append('<h1 id="h1"></h1>');
+
+      var el = document.createElement('h1');
+      var base = Toc.helpers.generateUniqueId(el);
+      expect(base).to.eql('h1-1');
     });
   });
 });

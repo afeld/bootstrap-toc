ad987471b47a8aa66a00789d292c8c18072698e7
Loris Bettazza <loris.bettazza@gmail.com>
2021-01-08 03:16:42 +0100

    Partial 2

--- bootstrap-toc.js
+++ bootstrap-toc.js
@@ -73,9 +73,9 @@
         return ul;
       },
 
-      createChildNavList: function($parent) {
+      createChildNavList: function(parent) {
         var childList = this.createNavList();
-        $parent.append(childList);
+        parent.appendChild(childList);
         return childList;
       },
 
@@ -124,25 +124,25 @@
 
       populateNav: function($topContext, topLevel, $headings) {
         var $context = $topContext;
-        var $prevNav;
+        var prevNav;
 
         var helpers = this;
         $headings.each(function(i, el) {
-          var $newNav = $(helpers.generateNavItem(el));
+          var newNav = helpers.generateNavItem(el);
           var navLevel = helpers.getNavLevel(el);
 
           // determine the proper $context
           if (navLevel === topLevel) {
             // use top level
             $context = $topContext;
-          } else if ($prevNav && $context === $topContext) {
+          } else if (prevNav && $context === $topContext) {
             // create a new level of the tree and switch to it
-            $context = $(helpers.createChildNavList($prevNav));
+            $context = $(helpers.createChildNavList(prevNav));
           } // else use the current $context
 
-          $context.append($newNav);
+          $context.append(newNav);
 
-          $prevNav = $newNav;
+          prevNav = newNav;
         });
       },
 
@@ -167,7 +167,7 @@
       // ensure that the data attribute is in place for styling
       opts.$nav.attr("data-toggle", "toc");
 
-      var $topContext = $(this.helpers.createChildNavList(opts.$nav));
+      var $topContext = $(this.helpers.createChildNavList(opts.$nav[0]));
       var topLevel = this.helpers.getTopLevel(opts.$scope);
       var $headings = this.helpers.getHeadings(opts.$scope, topLevel);
       this.helpers.populateNav($topContext, topLevel, $headings);
